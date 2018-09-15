7ee94588f444853a3a454b0123b9b22ffcfd5229
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-09-15 01:39:29 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,7 +35,7 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 shipping_manifest["muskets"] = 2
-shipping_manifest["gun_powder"] = 4
+shipping_manifest["gun powder"] = 4
 
 return shipping_manifest
 
