ed151aeac88205f322bbe3187a9502343f07e7a6
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-09-15 01:15:02 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,10 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  fruit_basket = {
+    "a" => "apples"
+    "b" => "bananas"
+    "c" => "cherry"
+   }
 end
 
 
